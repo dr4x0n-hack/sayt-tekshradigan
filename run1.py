8c8d9e812fdb7d876c450dd7e3c367e3093432bf
dr4x0n <dr4x0n14@gmail.com>
2025-12-06 04:21:09 +1400

    Create run1.py

---
+++ run1.py
@@ -0,0 +1,11 @@
+# Oddiy ishga tushirish
+python3 kali_pentest_gui.py
+
+# Sudo bilan (nmap uchun)
+sudo python3 kali_pentest_gui.py
+
+# Fullscreen rejimda
+python3 kali_pentest_gui.py --fullscreen
+
+# Log fayli bilan
+python3 kali_pentest_gui.py --log debug.log
